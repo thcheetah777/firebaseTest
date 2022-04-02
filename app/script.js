1ede286f58b503ec81dcef5b203b4532b2dc9f07
thcheetah777 <thcheetah777@gmail.com>
2022-04-01 18:07:07 -0700

    Get data firebase

--- app/script.js
+++ app/script.js
@@ -5,13 +5,19 @@ The main script for Firebase Test.
 *^*^*^*^*^*^*^*/
 
 import {firebaseConfig} from "../firebaseConfig.js";
-import {initializeApp} from "firebase/app";
-import firebase from "firebase";
-import {getFirestore} from "firebase/firestore";
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+import {collection, addDoc, getFirestore, doc, getDoc} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js";
+
 export class Game extends Phaser.Scene {
   constructor() {
     super("Game");
-    this.firebaseApp = initializeApp(firebaseConfig);
+    this.firebase = initializeApp(firebaseConfig);
+    const database = getFirestore(this.firebase);
+    (async () => {
+      const docRef = doc(database, "data", "data");
+      const docSnap = await getDoc(docRef);
+      console.log(docSnap.data());
+    })();
   }
   preload() {
 
